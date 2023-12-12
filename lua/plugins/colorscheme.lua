33d2d5b5da95209f93bb5939e82e356a99bfb8d1
Franco Pipolo <pipolofranco.dev@gmail.com>
2023-12-12 00:20:50 -0300

    agrego logo

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,42 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  { "sainnhe/sonokai" },
-  { "joshdick/onedark.vim" },
-  -- Configure LazyVim to load gruvbox
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = {
+      transparent_background = true,
+      flavour = "mocha",
+    },
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+  },
+  {
+    "miikanissi/modus-themes.nvim",
+    name = "modus",
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 1000,
+  },
+  {
+    "xiyaowong/transparent.nvim",
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
