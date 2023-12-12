45f78f8e208236c3c9e03353c4e2dcabf8b27167
Franco Pipolo <pipolofranco.dev@gmail.com>
2023-12-12 18:09:28 -0300

    agrego cambios

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,42 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    priority = 1000,
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
+    opts = {
+      transparent_background = true,
+      flavour = "mocha",
+    },
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-dragon",
+      colorscheme = "tokyonight",
     },
   },
 }
