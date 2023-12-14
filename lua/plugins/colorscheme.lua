a8baee99539c17fe9ff35b9056bd7b28c5f3088b
Franco Pipolo <pipolofranco.dev@gmail.com>
2023-12-13 21:33:41 -0300

    Modifico configuracion

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,42 +1,15 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    opts = {
-      transparent_background = true,
-      flavour = "mocha",
-    },
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-      notify = false,
-      mini = {
-        enabled = true,
-        indentscope_color = "",
-      },
-      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
+    "xiyaowong/nvim-transparent",
   },
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
+    "dracula/vim",
+    -- priority = 10000,
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "dracula",
     },
   },
 }
